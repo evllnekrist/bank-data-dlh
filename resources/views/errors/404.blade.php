6c5f1120f7c2e931a73dd01d49d75286e2dfe373
Evelline <elinyine@gmail.com>
2024-05-08 14:26:56 +0700

    fix [2]

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -9,9 +9,9 @@
     <div class="mt-10">	
         <center>
             <img src="{{asset('img/'.$collection[$random_key])}}" class="img-fluid" style="height:40vh" alt="">
-            <h1 class="text-lg font-medium mt-10">{{$title}}</h1>
+            <h1 class="text-lg font-medium mt-10">{{@$title}}</h1>
             <p class="text-xs">
-                {{$desc}}<br>
+                {{@$desc}}<br>
                 <a href="{{route('file-manager')}}">Kembali ke <u class="text-primary"><b>laman utama</b></u></a>
             </p>
         </center>
